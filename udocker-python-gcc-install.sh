7a0b927b863ac7e939a641d6b657b85904eb883d
Yuval Yehuda Barak <yuvalyehudab@WIN.CS.TAU.AC.IL>
2020-10-26 13:38:10 +0200

    add comments about working directory part

--- udocker-python-gcc-install.sh
+++ udocker-python-gcc-install.sh
@@ -3,9 +3,17 @@
 user=$(whoami)
 BASE_DIR=/specific/scratches/scratch
 
+# set working directory
+# default: $(BASE_DIR)/$(user)/udocker
+# optional: $1/udocker
+# only if udocker working directory was not set explicitly by user/shell
+
+# test:
+# run script
+# echo $UDOCKER_DIR
 if [[ -z $UDOCKER_DIR]]; then
     if [[ -z $1 ]]; then
-        export UDOCKER_DIR=$(BASE_DIR)/$user/udocker
+        export UDOCKER_DIR=$(BASE_DIR)/$(user)/udocker
     else
         export UDOCKER_DIR=$1/udocker
     fi
