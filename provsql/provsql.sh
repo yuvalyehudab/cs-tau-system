0c7617f37bb95322adad06ef399de7ac575604b0
yuvalyehudab <57196095+yuvalyehudab@users.noreply.github.com>
2021-04-05 12:22:50 +0300

    set execute bit for psql.sh, insert scripts

--- provsql/provsql.sh
+++ provsql/provsql.sh
@@ -2,12 +2,19 @@
 
 UDOCKER_DIR=/vol/scratch/yuvalyehudab/udocker
 export UDOCKER_DIR
+
+echo	'loading ...'
+echo	'chmod psql.sh'
+chmod u+x ./psql.sh
+echo	'pull container'
+echo	'pwd: ${PWD}'
+
 udocker pull ubuntu:20.10 && \
 	echo 'pulled' && \
 	udocker create --name=psql ubuntu:20.10 && \
 	echo 'created' && \
 	udocker setup --execmode=P2 psql && \
-	udocker run --containerauth --publish=2345:5432 psql psql.sh && \
+	udocker run --containerauth --publish=2345:5432 --volume=`pwd`:/scripts psql /scripts/psql.sh && \
 	echo 'installed psql'
 echo 'now udocker run --containerauth --publish=2345:5432 psql'
 echo 'then service postgres restart'
