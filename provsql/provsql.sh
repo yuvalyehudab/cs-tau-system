84fb30e1e9e94ef1b598a92d6b063b01fbbe3246
Yuval Barak <yuvalyehudab@mail.tau.ac.il>
2021-04-19 16:58:31 +0300

    updates

--- provsql/provsql.sh
+++ provsql/provsql.sh
@@ -5,6 +5,7 @@ export UDOCKER_DIR
 
 echo	'loading ...'
 
+SCRIPT_DIR=`dirname $0`
 CONTAINERS=`udocker ps | grep 'psql'`
 
 if [[ -z ${CONTAINERS} ]]; then
@@ -22,18 +23,18 @@ if [[ -z ${CONTAINERS} ]]; then
 	echo	'pull container ubuntu:latest. NOTE: last check on ubuntu 20.10'
 	echo	'latest ubuntu version is supposed to be:'
 	echo	${RELEVANT_YEAR}.${RELEVANT_MONTH}
-	echo	'udocker run --containerauth --publish=2345:5432 --volume=`pwd`:/scripts psql bash /scripts/psql.sh'
+	echo	'udocker run --containerauth --publish=2345:5432 --volume=${SCRIPT_DIR}:/scripts psql bash /scripts/psql.sh'
 	echo	'leave the terminal open. connect from another terminal on the same server with: '
 	echo	'conda create -n check_conn / conda create --prefix=/path/to/env/check_conn'
 	echo	'conda activate check_conn'
 	echo	'conda install psycopg2'
 	echo 	'python'
 	echo	'import psycopg2'
-	echo	'conn = psycopg2.connect("dbname=postgres user=postgres password=123 host=127.0.0.1 port=2345")'
+	echo	'conn = psycopg2.connect(database="postgres", user="postgres", password=123, host="127.0.0.1", port=2345)'
 	echo	'print(conn)'
 
 	udocker pull ubuntu:latest > /dev/null 2>&1
 	udocker create --name=psql ubuntu:latest
 	udocker setup --execmode=P2 psql
-	udocker run --containerauth --publish=2345:5432 --volume=`pwd`:/scripts psql bash /scripts/psql.sh
+	udocker run --containerauth --publish=2345:5432 --volume=`pwd`/${SCRIPT_DIR}:/scripts psql bash /scripts/psql.sh
 fi
--- provsql/psql.sh
+++ provsql/psql.sh
@@ -3,17 +3,25 @@
 DEBIAN_FRONTEND=noninteractive
 export DEBIAN_FRONTEND
 
-if [[ -z ${POSTGRES_PASSWORD}]]; then
+if [[ -z ${POSTGRES_PASSWORD} ]]; then
     export POSTGRES_PASSWORD=123
 fi
 
-apt update && apt upgrade -y
-apt install -y less git wget curl vim nano build-essential python3 libssl-dev dropbear postgresql postgresql-contrib
+echo "========== 1 =========" &&
+echo "========== 2 ========="
 
-su postgres -c 'pg_createcluster 12 main -- --username=postgres --pwfile=<(echo ${POSTGRES_PASSWORD})'
-chmod 0600 /etc/ssl/private/ssl-cert-snakeoil.key
-su postgres -c 'pg_ctlcluster 12 main start'
+# echo "========== 1 ========="
+echo "========== 3 ========="
+apt update && apt upgrade -y > /dev/null 2>&1 && \
+apt install -y less git wget curl vim nano build-essential python3 libssl-dev dropbear postgresql postgresql-contrib > /dev/null 2>&1 ;
+echo "========== 4 =========" && \
+su postgres -c 'pg_createcluster 12 main -- --username=postgres --pwfile=<(echo ${POSTGRES_PASSWORD})';
+echo "========== 4.5 ========="
+chmod 0600 /etc/ssl/private/ssl-cert-snakeoil.key &&
+echo "========== 5 =========" &&
+su postgres -c 'pg_ctlcluster 12 main start';
 
 # https://stackoverflow.com/a/18664239
 
-su postgres -c 'service postgresql restart'
+su postgres -c 'service postgresql restart';
+echo 'to exit, pls run: service postgresql stop' 
