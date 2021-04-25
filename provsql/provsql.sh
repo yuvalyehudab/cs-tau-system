e4b33bfea265cadca4e01053c2120c860930945f
Yuval Barak <yuvalyehudab@mail.tau.ac.il>
2021-04-25 16:18:08 +0300

    wrap up before writing guide

--- provsql/provsql.sh
+++ provsql/provsql.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-UDOCKER_DIR=/vol/scratch/`whoami`/udocker
+if [[ -z ${UDOCKER_DIR} ]]; then
+	UDOCKER_DIR=/vol/scratch/`whoami`/udocker
+fi
 export UDOCKER_DIR
 
 echo	'loading ...'
@@ -9,6 +10,14 @@ SCRIPT_DIR=`dirname $0`
 CONTAINERS=`udocker ps | grep 'psql'`
 
 if [[ -z ${CONTAINERS} ]]; then
+	echo
+	echo	"running 'from scratch'. pulling, creating and running container"
+	echo	"also, installing postgres on the container"
+	echo
+	echo	"assuming run from directory cs-tau-system/ or provsql/"
+	echo
+	sleep 0.1
+
 	RELEVANT_MONTH=`date +"%m"`
 	RELEVANT_YEAR=`date +"%y"`
 	if [ ${RELEVANT_MONTH} -le 10 ]; then
@@ -20,14 +29,20 @@ if [[ -z ${CONTAINERS} ]]; then
 	else
 		RELEVANT_MONTH="10"
 	fi
-	echo	"pull container ubuntu:latest. NOTE: last check on ubuntu 20.10"
+	echo	"pulling container ubuntu:latest. NOTE: last checked on ubuntu 20.10"
 	echo	"latest ubuntu version is supposed to be: ${RELEVANT_YEAR}.${RELEVANT_MONTH}"
+	echo
+	echo	"running with:"
 	echo	"udocker run --containerauth --publish=2345:5432 --volume=${SCRIPT_DIR}:/scripts psql bash /scripts/psql.sh"
-	echo	"leave the terminal open. connect from another terminal on the same server with: "
+	echo	"you can stop with:"
+	echo	'udocker run --containerauth --publish=2345:5432 --volume=${UDOCKER_DIR}/../cs-tau-system/provsql:/scripts psql bash /scripts/stop-server.sh'
+	echo	"leave the terminal open. connect from another terminal on the same server with (also in conn_check.py): "
+	echo
 	echo	"$ conda create -n check_conn or $ conda create --prefix=/path/to/env/check_conn"
 	echo	"$ conda activate check_conn"
 	echo	"$ conda install psycopg2"
 	echo 	"$ python"
+	echo
 	echo	">>> import psycopg2"
 	echo	">>> conn = psycopg2.connect(database='postgres', user='postgres', password=123, host='127.0.0.1', port=2345)"
 	echo	">>> print(conn)"
@@ -36,4 +51,16 @@ if [[ -z ${CONTAINERS} ]]; then
 	udocker create --name=psql ubuntu:latest
 	udocker setup --execmode=P2 psql
 	udocker run --containerauth --publish=2345:5432 --volume=`pwd`/${SCRIPT_DIR}:/scripts psql bash /scripts/psql.sh
+	echo
+else
+	echo	"looks like the container already exist."
+	echo
+	echo	"to delete you can:"
+	echo	'rm -rf ${UDOCKER_DIR}'
+	echo
+	echo	"to run with existing container you can:"
+	echo	'udocker run --containerauth --publish=2345:5432 --volume=${UDOCKER_DIR}/../cs-tau-system/provsql:/scripts psql bash /scripts/run-server.sh'
+	echo
+	echo	"for more complicated cases, mail system@cs.tau.ac.il and link to the github repository"
+	echo
 fi
--- provsql/psql.sh
+++ provsql/psql.sh
@@ -7,21 +7,21 @@ if [[ -z ${POSTGRES_PASSWORD} ]]; then
     export POSTGRES_PASSWORD=123
 fi
 
-echo "========== 1 =========" &&
-echo "========== 2 ========="
-# may add some sleeps?
-echo "========== 3 ========="
 apt update > /dev/null 2>&1 && apt upgrade -y > /dev/null 2>&1 && \
 apt install -y less git wget curl vim nano build-essential python3 libssl-dev dropbear postgresql postgresql-contrib > /dev/null 2>&1 ;
-echo "========== 4 =========" && \
+
 su postgres -c "pg_createcluster 12 main -- --username=postgres --pwfile=<(echo ${POSTGRES_PASSWORD})";
-echo "========== 4.5 ========="
-chmod 0600 /etc/ssl/private/ssl-cert-snakeoil.key &&
-echo "========== 5 =========" &&
+
+chmod 0600 /etc/ssl/private/ssl-cert-snakeoil.key
 su postgres -c 'pg_ctlcluster 12 main start';
 
 # https://stackoverflow.com/a/18664239
 
 su postgres -c 'service postgresql restart';
-echo 'to exit, pls run: service postgresql stop' 
-echo 'or: su postgres -c "pg_ctlcluster 12 main stop"'
+echo    'to exit, pls run: service postgresql stop' 
+echo    'or: su postgres -c "pg_ctlcluster 12 main stop"'
+echo    'or: bash /scripts/stop-server.sh'
+echo
+echo    "if you are not inside the container, use:"
+echo    'docker run --containerauth --publish=2345:5432 --volume=${UDOCKER_DIR}/../cs-tau-system/provsql:/scripts psql bash /scripts/stop-server.sh'
+echo
